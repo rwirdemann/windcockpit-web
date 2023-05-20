ff2f1c2279693e30be3e7cae99c2aa7ee1cc38df
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2023-05-20 12:10:31 +0200

    Fixes formatted duration

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
-  def formatted_duration(total_minutes)
-    total_minutes = total_minutes.round
-    hours = total_minutes / (60)
-    minutes = (total_minutes) % 60
-    seconds = total_minutes * 60 % 60
+  def formatted_duration(total_seconds)
+    total_seconds = total_seconds.round
+    hours = total_seconds / (60 * 60)
+    minutes = (total_seconds / 60) % 60
+    seconds = total_seconds % 60
     [hours, minutes, seconds].map do |t|
       t.round.to_s.rjust(2, '0')
     end.join(':')
