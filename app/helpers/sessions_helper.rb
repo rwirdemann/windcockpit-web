c0a2b4599465077ca1e3cbf075f7da4168248c0e
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2023-05-20 12:06:14 +0200

    Shows formatted duration

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  def formatted_duration(total_minutes)
+    total_minutes = total_minutes.round
+    hours = total_minutes / (60)
+    minutes = (total_minutes) % 60
+    seconds = total_minutes * 60 % 60
+    [hours, minutes, seconds].map do |t|
+      t.round.to_s.rjust(2, '0')
+    end.join(':')
+  end
 end
